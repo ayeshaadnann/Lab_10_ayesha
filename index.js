2c1d4837f6f3dfd31a18b6e5ffbc46f1b1019b7f
ayeshaadnann <ayesha.adnan1999@gmail.com>
2022-04-12 17:35:07 -0400

    commit

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const mongoose = require ("mongoose");
-require("dotenv") .config() ;
-const uri = process.env.ATLAS _URI;
-console. log(uri);
-mongoose. connect (uri, { useUnifiedTopology: true });
-const db = mongoose. connection;
-db. once ("open", () => {
-console. log ("Successfully connected to MongoDB using Mongoose!");
+
+
+require("dotenv").config();
+
+
+const uri = process.env.ATLAS_URI;
+
+console.log(uri);
+
+mongoose.connect(uri, { useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.once ("open", () => {
+console.log ("Successfully connected to MongoDB using Mongoose!");
+});
