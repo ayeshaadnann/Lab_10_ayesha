083b6102aaa9707ae226a444cbb0c2a698e16649
ayeshaadnann <ayesha.adnan1999@gmail.com>
2022-04-12 17:10:22 -0400

    env

---
+++ index.js
@@ -0,0 +1,8 @@
+const mongoose = require ("mongoose");
+require("dotenv") .config() ;
+const uri = process.env.ATLAS _URI;
+console. log(uri);
+mongoose. connect (uri, { useUnifiedTopology: true });
+const db = mongoose. connection;
+db. once ("open", () => {
+console. log ("Successfully connected to MongoDB using Mongoose!");
